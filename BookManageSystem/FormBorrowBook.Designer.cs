8aca10b77ab9a44d0bc74a5780f9e2d963394974
家熠 茅 <Mao492471757@outlook.com>
2025-03-07 16:33:12 +0800

    Nothing

--- BookManageSystem/FormBorrowBook.Designer.cs
+++ BookManageSystem/FormBorrowBook.Designer.cs
@@ -42,29 +42,33 @@
             this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cobNum = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(45, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.Size = new System.Drawing.Size(137, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "当前选中:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(484, 755);
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(484, 797);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(169, 57);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "租借";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // dgv
             // 
@@ -82,7 +86,7 @@
             this.Column8,
             this.Column9,
             this.Column10});
-            this.dgv.Location = new System.Drawing.Point(48, 77);
+            this.dgv.Location = new System.Drawing.Point(48, 119);
             this.dgv.Margin = new System.Windows.Forms.Padding(2);
             this.dgv.Name = "dgv";
             this.dgv.RowHeadersWidth = 82;
@@ -153,47 +157,57 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(800, 755);
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(800, 797);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(169, 57);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "返回";
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // label2
+            // lblName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(152, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 18);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "NULL";
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblName.ForeColor = System.Drawing.Color.Red;
+            this.lblName.Location = new System.Drawing.Point(185, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(93, 37);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "NULL";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 43);
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(45, 66);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 18);
+            this.label3.Size = new System.Drawing.Size(81, 37);
             this.label3.TabIndex = 5;
             this.label3.Text = "数量:";
             // 
-            // comboBox1
+            // cobNum
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(104, 40);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 26);
-            this.comboBox1.TabIndex = 6;
+            this.cobNum.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cobNum.FormattingEnabled = true;
+            this.cobNum.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cobNum.Location = new System.Drawing.Point(138, 63);
+            this.cobNum.Name = "cobNum";
+            this.cobNum.Size = new System.Drawing.Size(165, 45);
+            this.cobNum.TabIndex = 6;
             // 
             // FormBorrowBook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1435, 921);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cobNum);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dgv);
             this.Controls.Add(this.button1);
@@ -222,8 +236,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cobNum;
     }
 }
